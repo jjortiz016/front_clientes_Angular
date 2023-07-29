088670f81fb43261d6d9fb029a64542eb2aebf10
jjortiz016 <jjortiz016@misena.edu.co>
2023-07-28 21:08:02 -0500

    Se creo un nuevo directorio dentro de app llamado header y se coloco dentro de el enel nuevo componente llamado header.component.ts luego se importo y se coloco se llamo dentro del app.component.html

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    template: `
+    <h1> Angular Spring </h1>
+    `
+
+})
+export class HeaderComponent{
+
+}
