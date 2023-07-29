025c842448b63b526568448b9f327bdbfd4a4ce6
jjortiz016 <jjortiz016@misena.edu.co>
2023-07-28 19:10:08 -0500

    cambio en el index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a Angular';
+
+  curso: string=' Curso Spring con Angular';
+  alumno: string=' Jhon Jairo Ortiz';
 }
