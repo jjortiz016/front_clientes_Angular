b8680df2edad7c9d47d0febec474f8072c313538
jjortiz016 <jjortiz016@misena.edu.co>
2023-08-01 18:31:30 -0500

    Creando el listado los objeto del tipo Cliente, se creo de un card una tabla con el *ngFor y se modifico el app.component.html para llamar el componente dentro de clientes.component.ts creamos un array con el listado

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
 
 })
 export class ClientesComponent {
+  clientes: Cliente[] = [
+    {id:1, nombre: 'Jhon', apellido: 'Ortiz', email: 'email@hotmail.com', createAt:'2023-08-01'},
+    {id:2, nombre: 'Claudia', apellido: 'Gimenez', email: 'email@gmail.com', createAt:'2023-08-02'},
+    {id:3, nombre: 'Roberto', apellido: 'Mendoza', email: 'email@yahoo.com', createAt:'2023-08-01'}
+
+  ];
+
     constructor() {}
 }
