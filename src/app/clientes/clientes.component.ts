d2ca01e25e27b8f7e25f3119108c959362e85c91
jjortiz016 <jjortiz016@misena.edu.co>
2023-08-02 14:45:25 -0500

    Se implemento el observable en cliente.service.ts y se suscribio en el clientes.component.ts

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 
-
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -14,6 +13,11 @@ export class ClientesComponent implements OnInit {
     constructor(private clienteService: ClienteService) {}
 
     ngOnInit(){
-      this.clientes= this.clienteService.getClientes();
+    //  this.clientes= this.clienteService.getClientes();
+      this.clienteService.getClientes().subscribe(
+        clientes => this.clientes=clientes
+
+      );
+
     }
 }
