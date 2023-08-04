98d0597583026be2ec2d839ec838cf4c2a263c5d
jjortiz016 <jjortiz016@misena.edu.co>
2023-08-04 18:41:27 -0500

    Se modifico el angular.json ya que no estaba configurado para encuntrar las rutas de bootstrap y jquery, se modifico el app.module.ts con el HttpClientModule y el cliente.service para que consulte api rest

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable, of} from 'rxjs';
-
-
+import { HttpClient } from '@angular/common/http'; 
+import {map} from 'rxjs/operators'; //segunda forma
 @Injectable()
 export class ClienteService {
+  private urlEndPoint:string= 'http://localhost:8086/api/clientes';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+ // return of(CLIENTES);
+     //return this.http.get<Cliente[]>(this.urlEndPoint); //esta es una forma
+     return this.http.get(this.urlEndPoint).pipe(
+        map( (response) => response as Cliente[])
+      )  //segunda forma
+      
   }
 }
