5fe120eeaaae0c70aac025f0c2a55e043b33273a
jjortiz016 <jjortiz016@misena.edu.co>
2023-08-10 19:35:54 -0500

    Se creo el metodo create de tipo obsevable.

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable, of} from 'rxjs';
-import { HttpClient } from '@angular/common/http'; 
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
 import {map} from 'rxjs/operators'; //segunda forma
 @Injectable()
 export class ClienteService {
   private urlEndPoint:string= 'http://localhost:8086/api/clientes';
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private http: HttpClient) { }
 
@@ -17,5 +18,10 @@ export class ClienteService {
         map( (response) => response as Cliente[])
       )  //segunda forma
       
+  }
+
+  create(cliente: Cliente): Observable<Cliente>{
+      return this.http.post<Cliente>(this.urlEndPoint,cliente, {headers: this.httpHeaders})
+
   }
 }
