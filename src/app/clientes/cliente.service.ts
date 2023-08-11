d237975616066f94c098df4b6d60638929fbf4d7
jjortiz016 <jjortiz016@misena.edu.co>
2023-08-11 14:45:28 -0500

    Se creo la funcion para editar el cliente y actualizarlo, se modificaron el app.module.ts, clientes.component.html (boton editar), clientes.service, form.component.ts, form.component.ts

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable, of} from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http'; 
@@ -23,5 +22,15 @@ export class ClienteService {
   create(cliente: Cliente): Observable<Cliente>{
       return this.http.post<Cliente>(this.urlEndPoint,cliente, {headers: this.httpHeaders})
 
+  }
+
+  getCliente(id): Observable<Cliente>{
+
+     return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+  }
+
+
+  update(cliente: Cliente):Observable<Cliente>{
+     return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers:this.httpHeaders})
   }
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,7 +1,7 @@
 import { Component , OnInit} from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute} from '@angular/router';
 import  Swal from 'sweetalert2';
 @Component({
   selector: 'app-form',
@@ -13,17 +13,42 @@ export class FormComponent implements OnInit {
   cliente: Cliente = new Cliente()
   titulo:string = "Crear Cliente"
 
-  constructor(private clienteService: ClienteService, private router:Router){}   //inyectamos la dependencia
+  constructor(private clienteService: ClienteService, 
+    private router:Router,
+    private activateRoute: ActivatedRoute){}   //inyectamos la dependencia
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cargarCliente()
+
+  }
+ cargarCliente(): void{
+   this.activateRoute.params.subscribe(params => {
+      let id= params['id']
+      if(id){
+        this.clienteService.getCliente(id).subscribe((cliente => this.cliente = cliente))
+      }
+
+   })
+ }
+
+
 
   public create():void{
      this.clienteService.create(this.cliente)
         .subscribe(cliente => {
               this.router.navigate(['/clientes'])
-              Swal.fire('Nuevo cliente',`Cliente ${cliente.nombre} creadp con éxito!`, 'success' )
+              Swal.fire('Nuevo cliente',`Cliente ${cliente.nombre} creado con éxito!`, 'success' )
         }
        )
+  }
+
+  update(): void{
+    this.clienteService.update(this.cliente)
+    .subscribe(cliente => {
+       this.router.navigate(['/clientes'])
+       Swal.fire('Actualizar cliente',`Cliente ${cliente.nombre} actualizado con éxito!`, 'success' )
+    })
+
   }
 }
 
