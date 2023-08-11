5d37fb5a3f3b21809dfcbda9b9b1ac3201c73620
jjortiz016 <jjortiz016@misena.edu.co>
2023-08-11 18:15:38 -0500

    Se creo metodos de eliminar

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import  Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -19,5 +20,31 @@ export class ClientesComponent implements OnInit {
 
       );
 
+    }
+
+    delete (cliente: Cliente): void {
+      Swal.fire({
+        title: 'Esta seguro?',
+        text: `Seguro que desea elimina al cliente ${cliente.nombre} ${cliente.apellido}? `,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminarlo!',
+        cancelButtonText: 'No, cancelar!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.clienteService.delete(cliente.id).subscribe(  
+            response => {
+             this.clientes= this.clientes.filter(cli => cli !==cliente) //para que filtre el cliente en la lista de la tabla.
+              Swal.fire(
+                'Eliminado!',
+                `El cliente ${cliente.nombre} ha sido eliminado.`,
+                'success'
+              )
+            }
+          )
+        }
+      })
     }
 }
