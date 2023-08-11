25b19dcc455f5e396904c775794ed985c12b80a3
jjortiz016 <jjortiz016@misena.edu.co>
2023-08-10 21:43:45 -0500

    Se instalo sweetalert2 y se configuro en angular.json y en el form.component.ts, queda lanzando la alerta al guardar el cliente.

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -2,7 +2,7 @@ import { Component , OnInit} from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router';
-
+import  Swal from 'sweetalert2';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
@@ -15,13 +15,15 @@ export class FormComponent implements OnInit {
 
   constructor(private clienteService: ClienteService, private router:Router){}   //inyectamos la dependencia
 
-  ngOnInit(): void {
-      
-  }
+  ngOnInit(): void {}
+
   public create():void{
-     this.clienteService.create(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
-     )
+     this.clienteService.create(this.cliente)
+        .subscribe(cliente => {
+              this.router.navigate(['/clientes'])
+              Swal.fire('Nuevo cliente',`Cliente ${cliente.nombre} creadp con éxito!`, 'success' )
+        }
+       )
   }
 }
 
