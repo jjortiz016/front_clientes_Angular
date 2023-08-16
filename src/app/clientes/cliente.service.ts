4ed8e901d64f4aefbf6288cf881d13959ec48cb5
jjortiz016 <jjortiz016@misena.edu.co>
2023-08-15 20:45:44 -0500

    Manejo del error en el Frontend Angular en obtener por id se modifico el cliente.service.ts

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
-import { Observable, of} from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http'; 
-import {map} from 'rxjs/operators'; //segunda forma
+import {map, catchError} from 'rxjs/operators'; //segunda forma
+import { of, Observable, throwError} from 'rxjs';
+import  Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
 @Injectable()
 export class ClienteService {
   private urlEndPoint:string= 'http://localhost:8086/api/clientes';
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getClientes(): Observable<Cliente[]> {
  // return of(CLIENTES);
@@ -25,8 +28,15 @@ export class ClienteService {
   }
 
   getCliente(id): Observable<Cliente>{
-
-     return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+     return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+        catchError(e => {
+          this.router.navigate(['/clientes']); //para que enrute o redirija al listado de cliente
+          console.error(e.error.mensaje);
+          Swal.fire('Error al editar', e.error.mensaje , 'error' );
+          //return throwError(e);  //marca deprecated
+           return throwError(() => new Error(e))  //retornamos el el objeto del error e tipo observable
+        })
+     );
   }
 
 
