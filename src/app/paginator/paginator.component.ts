cadc6d422e4fc75bbf9588b5928295dc19d3d204
jjortiz016 <jjortiz016@misena.edu.co>
2023-10-01 18:12:51 -0500

    SE IMPLEMENTO RANGOS DE PAGINAS EN EL COMPONENTE PAGINADOR  y se utilizó el ngOnChanges para regrescar los rangos en el paginador.

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html'
  
 })
-export class PaginatorComponent implements OnInit { // clase hija
+export class PaginatorComponent implements OnInit, OnChanges { // clase hija
  @Input() paginador: any; //con el decorador @Input() inyectar el paginador en la clase hija
  
  paginas: number[];
-
+ desde: number;
+ hasta: number;
  constructor(){}
 
   ngOnInit(): void {
-      this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice+1);
+    this.initPaginator();
   }
+  ngOnChanges(changes: SimpleChanges): void {
+    let paginadorActualizado = changes['paginador'];
+    if(paginadorActualizado.previousValue){
+      this.initPaginator();
+    }
+    
+  }
+   private initPaginator():void{
+      this.desde=Math.min(Math.max(1,this.paginador.number-4), this.paginador.totalPages-5);
+      this.hasta=Math.max(Math.min(this.paginador.totalPages, this.paginador.number+4));
+
+      if(this.paginador.totalPages>5){
+        this.paginas = new Array(this.hasta - this.desde +1).fill(0).map((_valor, indice) => indice+this.desde);
+      }else{
+          this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice+1);
+      }
+
+   }
 
 }
