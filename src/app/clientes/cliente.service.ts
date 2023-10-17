06fd0c04a28314abb8dbc01c7bab4fe611aeb3a6
jjortiz016 <jjortiz016@misena.edu.co>
2023-10-16 22:03:42 -0500

    implementando  la subida del archivo en el front-end

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -109,4 +109,19 @@ export class ClienteService {
       })
     );
   }
+
+
+  subirFoto(archivo: File, id): Observable<Cliente>{
+     let formData = new FormData();
+     formData.append("archivo", archivo);
+     formData.append("id",id);
+     return this.http.post(`${this.urlEndPoint}/upload/`, formData).pipe(
+        map((Response:any)=> Response.cliente as Cliente),
+        catchError( e=> {
+          console.error(e.error.mensaje);
+          Swal.fire(e.error.mensaje, e.error.error, 'error' );
+           return throwError(() => new Error(e));
+        })
+     );
+  }
 }
--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute } from '@angular/router';
+import  Swal from 'sweetalert2';
 
 @Component({
   selector: 'detalle-cliente',
@@ -12,17 +13,37 @@ export class DetalleComponent {
 
   cliente: Cliente;
   titulo: string = "Detalle del cliente";
+  private fotoSeleccionada: File;
 
 
-  constructor(private clienteService: ClienteService, private activatedRote: ActivatedRoute){}
+  constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute){}
   ngOnInit() {
-    this.activatedRote.paramMap.subscribe(params => {
+    
+    this.activatedRoute.paramMap.subscribe(params => {
       let id:number = +params.get('id');
       if (id) {
         this.clienteService.getCliente(id).subscribe(cliente =>{
            this.cliente = cliente;
-        })
+        });
       }
     })
+  }
+
+  seleccionarFoto(event){
+     this.fotoSeleccionada = event.target.files[0];
+     console.log(this.fotoSeleccionada);
+
+  }
+  subirFoto(){
+    this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id)
+    .subscribe(cliente => {
+        this.cliente = cliente;
+        Swal.fire(
+          'Guardada!',
+          `La foto   ${cliente.foto} ha sido subidad correctamente.`,
+          'success'
+        )
+    })
+   
   }
 }
