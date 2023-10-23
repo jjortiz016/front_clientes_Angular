ba830755dfb1fc231a78017c2f5bcc134ea9fa73
jjortiz016 <jjortiz016@misena.edu.co>
2023-10-23 15:19:35 -0500

    Añadiendo barra de progreso se modificaron los archivos detalle.component.ts. detalle.component.html y cliente.service.ts.

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
-import { HttpClient, HttpHeaders } from '@angular/common/http'; 
+import { HttpClient,  HttpHeaders, HttpRequest, HttpEvent } from '@angular/common/http'; 
 import {map, catchError, tap} from 'rxjs/operators'; //segunda forma
 import { of, Observable, throwError} from 'rxjs';
 import  Swal from 'sweetalert2';
@@ -111,17 +111,15 @@ export class ClienteService {
   }
 
 
-  subirFoto(archivo: File, id): Observable<Cliente>{
+  subirFoto(archivo: File, id): Observable<HttpEvent<{}>>{
      let formData = new FormData();
      formData.append("archivo", archivo);
      formData.append("id",id);
-     return this.http.post(`${this.urlEndPoint}/upload`, formData).pipe(
-        map((Response:any)=> Response.cliente as Cliente),
-        catchError( e=> {
-          console.error(e.error.mensaje);
-          Swal.fire(e.error.mensaje, e.error.error, 'error' );
-           return throwError(() => new Error(e));
-        })
-     );
+
+    const req = new HttpRequest('POST', `${this.urlEndPoint}/upload`, formData, {
+        reportProgress: true
+    });
+
+     return this.http.request(req);
   }
 }
