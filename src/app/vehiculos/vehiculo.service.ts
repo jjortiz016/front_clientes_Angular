000b9b8f06a8a3af20888d5d6516f84a0b4db60f
jjortiz016 <jjortiz016@misena.edu.co>
2023-10-28 19:02:34 -0500

    se corrigio enlace de conexión  urlEndPoint en el vehiculo.service.ts.

--- src/app/vehiculos/vehiculo.service.ts
+++ src/app/vehiculos/vehiculo.service.ts
@@ -2,12 +2,23 @@ import { Injectable } from '@angular/core';
 import { VEHICULOS } from './vehiculos.json';
 import { Vehiculo } from './vehiculo';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class VehiculoService {
+  private urlEndPoint:string='http://localhost:8086/api2/vehiculos';
 
-  constructor() { }
-  getVehiculos(): Observable <Vehiculo[]> {
+  constructor(private http: HttpClient) { }
+  /*getVehiculos(): Observable <Vehiculo[]> {
     return of (VEHICULOS); // of convierte el objeto VEHICULOS en un observable
+  }*/
+
+  getVehiculos(): Observable<Vehiculo[]>{
+     return this.http.get(this.urlEndPoint).pipe(
+      map((response) => response as Vehiculo[])
+     )
   }
+
+
 }
