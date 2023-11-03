15198ba50989234537b86eae3c483b84288ab123
jjortiz016 <jjortiz016@misena.edu.co>
2023-11-02 22:17:33 -0500

    Se agrego boton de create en el formulario, se coloco el implements OnInit en la clase FormVehiculoComponent.

--- src/app/vehiculos/form-vehiculo.component.ts
+++ src/app/vehiculos/form-vehiculo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Vehiculo } from './vehiculo';
 import { VehiculoService } from './vehiculo.service';
 import { Router } from '@angular/router';
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './form-vehiculo.component.html',
 
 })
-export class FormVehiculoComponent {
+export class FormVehiculoComponent implements OnInit {
   vehiculo: Vehiculo = new Vehiculo();
   tituloVehiculo:string = 'Crear Vehiculo';
 
@@ -20,7 +20,7 @@ export class FormVehiculoComponent {
 
   public create():void{
     this.vehiculoService.create(this.vehiculo).subscribe(
-      response => this.router.navigate(['/vehiculos'])
+      response => this.router.navigate(['/vehiculos']) //retorna al listado de clientes
     )
   }
 
