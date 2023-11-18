ca336de65455f2bbca075dff868091a6de621cd4
jjortiz016 <jjortiz016@misena.edu.co>
2023-11-17 22:37:47 -0500

    Se implementaron  rangos de paginas en el paginatorvehiculo.component.ts quedó funcionando correctamente.

--- src/app/paginatorvehiculo/paginatorvehiculo.component.ts
+++ src/app/paginatorvehiculo/paginatorvehiculo.component.ts
@@ -8,17 +8,34 @@ import { Component, OnInit, Input, OnChanges,  SimpleChanges } from '@angular/co
  
   
 })
-export class PaginatorvehiculoComponent implements OnInit { //, OnChanges
+export class PaginatorvehiculoComponent implements OnInit, OnChanges { //, OnChanges
   @Input() paginadorvehiculo: any;
   paginas: number[];
 
+  desde: number;
+  hasta: number;
+
+
   constructor(){}
+  
   ngOnInit(): void {
- //   console.log("paginadorvehiculo.totalPages en paginatorvehiculo:", this.paginadorvehiculo.totalPages);
-      this.paginas = new Array(this.paginadorvehiculo.totalPages).fill(0).map((valor, indice)=> indice+1);      
-  }
-/*
-  ngOnChanges(changes: SimpleChanges): void {
+ 
       
-  }*/
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.desde= Math.min(Math.max(1, this.paginadorvehiculo.number-4), this.paginadorvehiculo.totalPages-5);
+    this.hasta= Math.max(Math.min(this.paginadorvehiculo.totalPages, this.paginadorvehiculo.number+4),6); 
+
+    if(this.paginadorvehiculo.totalPages>5){
+      this.paginas= new Array(this.hasta - this.desde+1).fill(0).map((_valor, indice)=> indice + this.desde);
+
+    }else{
+      this.paginas = new Array(this.paginadorvehiculo.totalPages).fill(0).map((valor, indice)=> indice+1);     
+    }
+    
+    
+     
+
+  }
 }
