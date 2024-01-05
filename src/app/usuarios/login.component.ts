cda7c2d1ae556b044fe5178b1691991f7fb868be
jjortiz016 <jjortiz016@misena.edu.co>
2024-01-05 15:46:18 -0500

    Añadiendo el metodo login() en el compomente y se creo la clase de usuario, se modifico el login.component.html para capturar los datos del usuario.

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Usuario } from './usuario';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,12 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo:string= "Please sign in";
+  usuario: Usuario;
 
   constructor(){
-
+    this.usuario = new Usuario(); //incializamos el usuario
   }
   ngOnInit(): void {
       
   }
+   login(): void{
+      console.log(this.usuario);
+      if(this.usuario.username ==  null || this.usuario.password == null){
+       
+        Swal.fire('Error Login', 'Username o password vacias!' , 'error' );
+        return;
+      }
+   }
 
 }
